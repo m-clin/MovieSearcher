fc595eff4b7a3e47584860081478523a83a16554
Marclin Abarracoso <marclinabarracoso2@gmail.com>
2023-12-30 12:41:01 +0800

    Added SimilarMovies function

--- moviedetails.js
+++ moviedetails.js
@@ -10,6 +10,27 @@ async function getMovieDetails() {
     return data;
 }
 
+async function getSimilarMovies() {
+    const response = await fetch(`${apiUrl}/movie/${movieId}/similar?api_key=${apiKey}`);
+    const data = await response.json();
+    console.log('Similar Movies Data:', data); // Log similar movies data to the console
+    displayMovies(data.results, 'similarMovies');
+}
+
+function displayMovies(movies, containerId) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = '';
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('similar-movie');
+        movieElement.innerHTML = `
+        <img src="${imageBaseUrl}${movie.poster_path}" alt="${movie.title}">
+        <h3>${movie.title}</h3>
+        `;
+        container.appendChild(movieElement);
+    });
+}
+
 function displayMovieDetails(movie) {
     const detailsContainer = document.getElementById('movieDetails');
     detailsContainer.style.display = 'block';
